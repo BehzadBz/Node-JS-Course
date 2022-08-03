e95a5c331e87320668038637f67cbf5b77d3c4fc
BehzadBz <royalblue.bbz@gmail.com>
2022-08-03 22:46:49 +0430

    modified:   1-node-farm/starter/index.js

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,4 +1,9 @@
 const fs = require("fs");
+const http = require("http");
+const url = require("url");
+
+///////////////////////////////
+// Files
 
 // const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
 // console.log(textIn);
@@ -9,17 +14,41 @@ const fs = require("fs");
 // console.log("File created");
 
 // async
-fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
-  fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
-    console.log(data2);
-    fs.readFile("./txt/append.txt", "utf-8", (err, data3) => {
-      console.log(data3);
+// fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
+//   if (err) return console.log("ERROR 💣");
 
-      fs.writeFile("./txt/final.txt", `${data2}\n${data3}`, "utf-8", (err) => {
-        console.log("Your file has been written 😊");
-      });
+//   fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
+//     console.log(data2);
+//     fs.readFile("./txt/append.txt", "utf-8", (err, data3) => {
+//       console.log(data3);
+
+//       fs.writeFile("./txt/final.txt", `${data2}\n${data3}`, "utf-8", (err) => {
+//         console.log("Your file has been written 😊");
+//       });
+//     });
+//   });
+// });
+
+// console.log("Will read file!");
+
+///////////////////////////////////////
+// Servers
+const server = http.createServer((req, res) => {
+  const pathName = req.url;
+
+  if (pathName === "/" || pathName === "/overview") {
+    res.end("This is OVERVIEW");
+  } else if (pathName === "/product") {
+    res.end("This is PRODUCT");
+  } else {
+    res.writeHead(404, {
+      "Content-type": "text/html",
+      "my-own-header": "hello-world",
     });
-  });
+    res.end("Page not found!");
+  }
 });
 
-console.log("Will read file!");
+server.listen(8000, "127.0.0.1", () => {
+  console.log("Listening to request on port 8000");
+});
