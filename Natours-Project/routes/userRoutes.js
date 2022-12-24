40e07f0835597bfed4c02874da324725fc1e9698
BehzadBz <royalblue.bbz@gmail.com>
2022-12-24 17:14:59 +0330

    Section 63 and refactoring our project

---
+++ Natours-Project/routes/userRoutes.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const usedController = require("../controllers/userController");
+
+const router = express.Router();
+
+router
+  .route("/")
+  .get(usedController.getAllUsers)
+  .post(usedController.createUser);
+
+router
+  .route("/:id")
+  .get(usedController.getUser)
+  .patch(usedController.updateUser)
+  .delete(usedController.deleteUser);
+
+module.exports = router;
